1fabe508a7c299c41e7a863b630fd27e706c7456
João-Victor-Ifanger <jvifanger12@gmail.com>
2025-12-19 14:06:45 -0300

    Criação de plataformas
    Criei plataformas que conseguem se mover pelo jogo e que consegue levar o jogador junto

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -12,14 +12,23 @@ var direction = 0
 #const JUMP_VELOCITY = -400.0
 
 
+func _input(event):
+	#Jump Event
+	if event.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_power * jump_multiplier
+	#Handle jump down
+	if event.is_action_pressed("move_down"):
+		set_collision_mask_value(10, false)
+	else:
+		set_collision_mask_value(10, true)
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_power * jump_multiplier
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
